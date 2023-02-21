a7f88aa65ce5126f69390fe1bb25ccab7fd30c88
Leonardo Simmons <leonardo.simmons07@gmail.com>
2023-02-21 07:57:19 -0800

    added aux content -> episode 1

--- script.js
+++ script.js
@@ -27,17 +27,17 @@ const EPISODE_COUNT_TOTAL = 8;
  */
 
 const EPISODE_LINKS = [
-    "https://www.youtube.com/embed/mKazCBAZHI4",
-    "https://www.youtube.com/embed/xA4Qwzq9-l8",
-    "https://www.youtube.com/embed/uLZJvnt7nZY",
-    "https://www.youtube.com/embed/NqoxANa8kL4",
-    "https://www.youtube.com/embed/EH4Dk2fGdFQ",
-    "https://www.youtube.com/embed/OnegTLpfGdc",
-    "https://www.youtube.com/embed/OnegTLpfGdc",
-    "https://www.youtube.com/embed/0OFliVeOkCY",
+    'https://www.youtube.com/embed/mKazCBAZHI4',
+    'https://www.youtube.com/embed/xA4Qwzq9-l8',
+    'https://www.youtube.com/embed/uLZJvnt7nZY',
+    'https://www.youtube.com/embed/NqoxANa8kL4',
+    'https://www.youtube.com/embed/EH4Dk2fGdFQ',
+    'https://www.youtube.com/embed/OnegTLpfGdc',
+    'https://www.youtube.com/embed/OnegTLpfGdc',
+    'https://www.youtube.com/embed/0OFliVeOkCY',
 ];
 
-const AUX_LINKS = [["www.google.com"]];
+const AUX_LINKS = [['https://www.youtube.com/embed/YLqHp0LKS8U']];
 
 /*
  * -----------------------
@@ -55,19 +55,19 @@ const Episodes = [];
  */
 
 /* Intro */
-const intro = document.querySelector(".aic__intro");
-const mainTitle = intro.querySelector("span");
-const scrollArrow = intro.querySelector(".aic__intro--scroll-arrow > img");
+const intro = document.querySelector('.aic__intro');
+const mainTitle = intro.querySelector('span');
+const scrollArrow = intro.querySelector('.aic__intro--scroll-arrow > img');
 
 /* Content */
-const contentTrigger = document.querySelector("#content-trigger");
-const content = document.querySelector(".aic__content");
-const instructions = content.querySelector("h2");
-const bgVideo = content.querySelector("video");
+const contentTrigger = document.querySelector('#content-trigger');
+const content = document.querySelector('.aic__content');
+const instructions = content.querySelector('h2');
+const bgVideo = content.querySelector('video');
 
 /* Modals */
-const modals = document.querySelector(".aic__modals");
-const closeModalBtn = document.querySelector("#aic__modals--btn-close");
+const modals = document.querySelector('.aic__modals');
+const closeModalBtn = document.querySelector('#aic__modals--btn-close');
 
 /*
  * -----------------------
@@ -102,7 +102,7 @@ function generateEpisodes() {
     const asset = (num, kind, type) => {
         return num <= EPISODE_COUNT_CURRENT
             ? `${BASE_URL}/ep-${num}-${kind}.${type}`
-            : "";
+            : '';
     };
 
     /* Generate episode section blocks & coresponding modal */
@@ -110,21 +110,21 @@ function generateEpisodes() {
         const episodeNum = index + 1;
 
         /* Builds individual episode section */
-        const section = document.createElement("section");
-        section.setAttribute("id", `aic__episode-${episodeNum}`);
-        section.classList.add("aic__ep--block");
+        const section = document.createElement('section');
+        section.setAttribute('id', `aic__episode-${episodeNum}`);
+        section.classList.add('aic__ep--block');
         section.style.height = `${blockHeight}px`;
         section.innerHTML = `
-            <div ${episodeNum > EPISODE_COUNT_CURRENT ? 'class="hidden"' : ""}>
+            <div ${episodeNum > EPISODE_COUNT_CURRENT ? 'class="hidden"' : ''}>
                 <img class="aic__ep--circle" src=${asset(
                     episodeNum,
-                    "circle",
-                    "png"
+                    'circle',
+                    'png',
                 )} />
                 <img class="aic__ep--title" src=${asset(
                     episodeNum,
-                    "title",
-                    "png"
+                    'title',
+                    'png',
                 )} />
             </div>
         </div>
@@ -132,46 +132,40 @@ function generateEpisodes() {
 
         /* Add connecting line if NOT last node */
         if (episodeNum < EPISODE_COUNT_CURRENT) {
-            const mainLine = document.createElement("img");
+            const mainLine = document.createElement('img');
             mainLine.classList.add(`aic__ep--line-main`);
             mainLine.setAttribute(
-                "src",
-                `${BASE_URL}/ep-${episodeNum}-main-line.png`
+                'src',
+                `${BASE_URL}/ep-${episodeNum}-main-line.png`,
             );
-            section.querySelector("div").append(mainLine);
+            section.querySelector('div').append(mainLine);
         }
 
-        /* Add Auxillary content */
-        //const auxContent = generateAuxContent(episodeNum, section);
+        /** Generate & Add Auxillary content */
+        const auxContent = generateAuxContent(episodeNum, section);
 
         /* Generate corresponding modal and add to page */
         const modal = generateEpisodeModal(episodeNum);
-
         modals.append(modal);
 
         /* Add event listeners */
-        const node = section.querySelector(".aic__ep--circle");
-        node.addEventListener("click", () => openEpisodeModal(modal));
-        modals.addEventListener("click", () => {
+        const node = section.querySelector('.aic__ep--circle');
+        node.addEventListener('click', () => openEpisodeModal(modal));
+        modals.addEventListener('click', () => {
             closeEpisodeModal(modal);
         });
 
         /* Adds episode section to the page */
-        content.querySelector(".aic__episodes").append(section);
+        content.querySelector('.aic__episodes').append(section);
 
         /* Adds episode data to 'Episodes' global */
         Episodes.push({
-            number: section,
-            element: {
-                circle: section.querySelector(".aic__ep--circle"),
-                line: {
-                    main: section.querySelector(".aic__ep--line-main"),
-                },
-                title: section.querySelector(".aic__ep--title"),
-                wrapper: section,
-                modal,
-            },
-            // auxContent,
+            circle: section.querySelector('.aic__ep--circle'),
+            line: section.querySelector('.aic__ep--line-main'),
+            title: section.querySelector('.aic__ep--title'),
+            section,
+            modal,
+            auxContent,
         });
     });
 }
@@ -183,29 +177,39 @@ function generateAuxContent(episodeNum, section) {
         AUX_LINKS.forEach((links) => {
             links.forEach((_, index) => {
                 const contentNum = index + 1;
-                const auxLine = document.createElement("img");
+                const auxLine = document.createElement('img');
                 auxLine.classList.add(`aic__ep--aux-line-${contentNum}`);
-                auxLine.setAttribute("src", `${BASE_URL}/aux-line.png`);
+                auxLine.setAttribute('src', `${BASE_URL}/aux-line.png`);
 
-                const auxDot = document.createElement("img");
+                const auxDot = document.createElement('img');
                 auxDot.classList.add(`aic__ep--aux-dot-${contentNum}`);
-                auxDot.setAttribute("src", `${BASE_URL}/aux-dot.png`);
+                auxDot.setAttribute('src', `${BASE_URL}/aux-dot.png`);
 
                 /* Add aux content to page */
-                section.querySelector("div").append(auxLine);
-                section.querySelector("div").append(auxDot);
+                section.querySelector('div').append(auxLine);
+                section.querySelector('div').append(auxDot);
+
+                /* Create Modal */
+                const modal = generateEpisodeModal(episodeNum, {
+                    type: 'aux',
+                    auxNum: contentNum,
+                });
+                modals.append(modal);
 
                 /* Add Event Listener */
+                const dot = section.querySelector(
+                    `.aic__ep--aux-dot-${contentNum}`,
+                );
+                auxDot.addEventListener('click', () => openEpisodeModal(modal));
+                modals.addEventListener('click', () => {
+                    closeEpisodeModal(modal);
+                });
 
-                const content = {
-                    dot: section.querySelector(
-                        `.aic__ep--aux-dot-${contentNum}`
-                    ),
-                    line: section.querySelector(
-                        `.aic__ep--aux-line-${contentNum}`
-                    ),
-                };
-                auxContent.push(content);
+                auxContent.push({
+                    dot: auxDot,
+                    line: auxLine,
+                    modal,
+                });
             });
         });
     }
@@ -214,23 +218,27 @@ function generateAuxContent(episodeNum, section) {
 }
 
 function generateEpisodeModal(episodeNum, opts) {
-    const modal = document.createElement("div");
-    modal.setAttribute("id", `aic__episode-${episodeNum}--modal`);
-    modal.classList.add("modal");
-    modal.classList.add("hidden");
+    const modal = document.createElement('div');
+    const id =
+        opts && opts.type === 'aux'
+            ? `aic__episode-${episodeNum}-aux-${opts.auxNum}-modal`
+            : `aic__episode-${episodeNum}--modal`;
+    modal.setAttribute('id', id);
+    modal.classList.add('modal');
+    modal.classList.add('hidden');
 
     modal.innerHTML = `
         <div class="aic__modals--wrapper">
             <iframe 
                 style="position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);width:${
                     viewport().width > BREAKPOINT_DESKTOP_SM
-                        ? "95%"
-                        : viewport().width * 0.95 + "px"
+                        ? '95%'
+                        : viewport().width * 0.95 + 'px'
                 };height:${
-        viewport().width > BREAKPOINT_DESKTOP_SM ? "100%" : "315px"
+        viewport().width > BREAKPOINT_DESKTOP_SM ? '100%' : '315px'
     };margin: 0 auto; padding: 5px;"  
                 src=${
-                    opts && opts.type === "aux"
+                    opts && opts.type === 'aux'
                         ? AUX_LINKS[episodeNum - 1][opts.auxNum - 1]
                         : EPISODE_LINKS[episodeNum - 1]
                 }
@@ -248,9 +256,9 @@ function generateEpisodeModal(episodeNum, opts) {
         </div>
     `;
 
-    const iframe = modal.querySelector("iframe");
-    iframe.addEventListener("keydown", (evt) => {
-        if (evt.key === "Escape" && !modal.classList.contains("hidden"))
+    const iframe = modal.querySelector('iframe');
+    iframe.addEventListener('keydown', (evt) => {
+        if (evt.key === 'Escape' && !modal.classList.contains('hidden'))
             closeEpisodeModal(modal);
     });
 
@@ -259,21 +267,21 @@ function generateEpisodeModal(episodeNum, opts) {
 
 function openEpisodeModal(modal) {
     if (
-        modals.classList.contains("hidden") &&
-        modal.classList.contains("hidden")
+        modals.classList.contains('hidden') &&
+        modal.classList.contains('hidden')
     ) {
-        modals.classList.remove("hidden");
-        modal.classList.remove("hidden");
+        modals.classList.remove('hidden');
+        modal.classList.remove('hidden');
     }
 }
 
 function closeEpisodeModal(modal) {
     if (
-        !modals.classList.contains("hidden") &&
-        !modal.classList.contains("hidden")
+        !modals.classList.contains('hidden') &&
+        !modal.classList.contains('hidden')
     ) {
-        modals.classList.add("hidden");
-        modal.classList.add("hidden");
+        modals.classList.add('hidden');
+        modal.classList.add('hidden');
     }
 }
 
@@ -299,7 +307,7 @@ function appearOnScroll(el, trigger, opts) {
         el,
         (opts && opts.tweenDuaration) || 5,
         { opacity: (opts && opts.startOpactiy) || 0 },
-        { opacity: (opts && opts.endOpactiy) || 1 }
+        { opacity: (opts && opts.endOpactiy) || 1 },
     );
 
     new ScrollMagic.Scene({
@@ -313,7 +321,7 @@ function appearOnScroll(el, trigger, opts) {
 }
 
 function draw(svg, trigger, opts) {
-    const path = svg.querySelector("path");
+    const path = svg.querySelector('path');
     preparePath(path);
 
     const tween = TweenMax.to(path, (opts && opts.tweenDuaration) || 3, {
@@ -336,7 +344,7 @@ function fadeOnScroll(el, trigger, opts) {
         el,
         (opts && opts.tweenDuaration) || 3,
         { opacity: (opts && opts.startOpactiy) || 1 },
-        { opacity: opts && opts.endOpactiy }
+        { opacity: opts && opts.endOpactiy },
     );
 
     new ScrollMagic.Scene({
@@ -356,14 +364,14 @@ function fadeOnScroll(el, trigger, opts) {
  */
 
 /* Closes episode modal when user presses the 'Escape' key */
-document.addEventListener("keydown", (evt) => {
-    if (evt.key === "Escape")
-        Episodes.forEach((episode) => closeEpisodeModal(episode.element.modal));
+document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape')
+        Episodes.forEach((episode) => closeEpisodeModal(episode.modal));
 });
 
 /* Enables 'close modal' button */
-closeModalBtn.addEventListener("click", () => {
-    Episodes.forEach((episode) => closeEpisodeModal(episode.element.modal));
+closeModalBtn.addEventListener('click', () => {
+    Episodes.forEach((episode) => closeEpisodeModal(episode.modal));
 });
 
 /*
@@ -394,20 +402,20 @@ function preparePath(path) {
 function viewport() {
     const vw = Math.max(
         document.documentElement.clientWidth || 0,
-        window.innerWidth || 0
+        window.innerWidth || 0,
     );
     const vh = Math.max(
         document.documentElement.clientHeight || 0,
-        window.innerHeight || 0
+        window.innerHeight || 0,
     );
 
     switch (true) {
         case vw < BREAKPOINT_TABLET_SM:
-            return { type: "mobile", width: vw, height: vh };
+            return { type: 'mobile', width: vw, height: vh };
         case vw > BREAKPOINT_DESKTOP_SM:
-            return { type: "desktop", width: vw, height: vh };
+            return { type: 'desktop', width: vw, height: vh };
         default:
-            return { type: "tablet", width: vw, height: vh };
+            return { type: 'tablet', width: vw, height: vh };
     }
 }
 
@@ -419,20 +427,20 @@ function viewport() {
 
 /* Main title intro */
 function introScene() {
-    const scrollText = intro.querySelector("div");
+    const scrollText = intro.querySelector('div');
 
     const timeline = new TimelineMax();
     const scrolLTextAnim = TweenMax.fromTo(
         scrollText,
         5,
         { opacity: 1 },
-        { opacity: 0 }
+        { opacity: 0 },
     );
     const textFadeAnim = TweenMax.fromTo(
         mainTitle,
         3,
         { opacity: 0.3 },
-        { opacity: 1 }
+        { opacity: 1 },
     );
     const textDirectionAnim = TweenMax.to(mainTitle, 0.5, {
         scale: 1.2,
@@ -443,7 +451,7 @@ function introScene() {
         scrollArrow,
         5,
         { opacity: 1 },
-        { opacity: 0 }
+        { opacity: 0 },
     );
 
     timeline
@@ -488,19 +496,29 @@ function introScene() {
 }
 
 function episodeOneScene() {
-    appearOnScroll(Episodes[0].element.circle, intro, {
+    appearOnScroll(Episodes[0].circle, intro, {
         duration: 100,
         offset: 280,
     });
 
-    appearOnScroll(Episodes[0].element.title, intro, {
+    appearOnScroll(Episodes[0].title, intro, {
         duration: 75,
         offset: 365,
     });
 
-    appearOnScroll(Episodes[0].element.line.main, intro, {
+    appearOnScroll(Episodes[0].line, intro, {
         duration: 200,
         offset: 330,
+    });
+
+    appearOnScroll(Episodes[0].auxContent[0].line, intro, {
+        duration: 250,
+        offset: 300,
+    });
+
+    appearOnScroll(Episodes[0].auxContent[0].dot, intro, {
+        duration: 100,
+        offset: 350,
     });
 
     appearOnScroll(instructions, intro, {
@@ -510,114 +528,114 @@ function episodeOneScene() {
 }
 
 function episodeTwoScene() {
-    appearOnScroll(Episodes[1].element.circle, intro, {
+    appearOnScroll(Episodes[1].circle, intro, {
         duration: 100,
         offset: 355,
     });
 
-    appearOnScroll(Episodes[1].element.title, intro, {
+    appearOnScroll(Episodes[1].title, intro, {
         duration: 75,
         offset: 440,
     });
 
-    appearOnScroll(Episodes[1].element.line.main, intro, {
+    appearOnScroll(Episodes[1].line, intro, {
         duration: 200,
         offset: 415,
     });
 }
 
 function episodeThreeScene() {
-    appearOnScroll(Episodes[2].element.circle, intro, {
+    appearOnScroll(Episodes[2].circle, intro, {
         duration: 100,
         offset: 430,
     });
 
-    appearOnScroll(Episodes[2].element.title, intro, {
+    appearOnScroll(Episodes[2].title, intro, {
         duration: 75,
         offset: 515,
     });
 
-    appearOnScroll(Episodes[2].element.line.main, intro, {
+    appearOnScroll(Episodes[2].line, intro, {
         duration: 200,
         offset: 490,
     });
 }
 
 function episodeFourScene() {
-    appearOnScroll(Episodes[3].element.circle, intro, {
+    appearOnScroll(Episodes[3].circle, intro, {
         duration: 100,
         offset: 505,
     });
 
-    appearOnScroll(Episodes[3].element.title, intro, {
+    appearOnScroll(Episodes[3].title, intro, {
         duration: 75,
         offset: 590,
     });
 
-    appearOnScroll(Episodes[3].element.line.main, intro, {
+    appearOnScroll(Episodes[3].line, intro, {
         duration: 200,
         offset: 565,
     });
 }
 
 function episodeFiveScene() {
-    appearOnScroll(Episodes[4].element.circle, intro, {
+    appearOnScroll(Episodes[4].circle, intro, {
         duration: 100,
         offset: 580,
     });
 
-    appearOnScroll(Episodes[4].element.title, intro, {
+    appearOnScroll(Episodes[4].title, intro, {
         duration: 75,
         offset: 665,
     });
 
-    appearOnScroll(Episodes[4].element.line.main, intro, {
+    appearOnScroll(Episodes[4].line, intro, {
         duration: 200,
         offset: 640,
     });
 }
 
 function episodeSixScene() {
-    appearOnScroll(Episodes[5].element.circle, intro, {
+    appearOnScroll(Episodes[5].circle, intro, {
         duration: 100,
         offset: 655,
     });
 
-    appearOnScroll(Episodes[5].element.title, intro, {
+    appearOnScroll(Episodes[5].title, intro, {
         duration: 75,
         offset: 715,
     });
 
-    appearOnScroll(Episodes[5].element.line.main, intro, {
+    appearOnScroll(Episodes[5].line, intro, {
         duration: 200,
         offset: 735,
     });
 }
 
 function episodeSevenScene() {
-    appearOnScroll(Episodes[6].element.circle, intro, {
+    appearOnScroll(Episodes[6].circle, intro, {
         duration: 100,
         offset: 730,
     });
 
-    appearOnScroll(Episodes[6].element.title, intro, {
+    appearOnScroll(Episodes[6].title, intro, {
         duration: 75,
         offset: 790,
     });
 
-    appearOnScroll(Episodes[6].element.line.main, intro, {
+    appearOnScroll(Episodes[6].line, intro, {
         duration: 200,
         offset: 805,
     });
 }
 
 function episodeEightScene() {
-    appearOnScroll(Episodes[7].element.circle, intro, {
+    appearOnScroll(Episodes[7].circle, intro, {
         duration: 100,
         offset: 760,
     });
 
-    appearOnScroll(Episodes[7].element.title, intro, {
+    appearOnScroll(Episodes[7].title, intro, {
         duration: 75,
         offset: 825,
     });
